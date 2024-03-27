ac07f318f92012c113e7d73e575e15ec3d49e4ec
kaixuan <3024432688@qq.com>
2024-03-27 12:15:11 +0800

    fourth commit

---
+++ assignments4/Clock/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Timers;
+
+class Clock
+{
+    DateTime alarmTime = DateTime.Now;
+    public delegate void AlarmHandler(object sender, DateTime args);
+    public delegate void TickHanlder(object sender, DateTime args);
+
+    public event AlarmHandler OnAlarm;
+    public event TickHanlder OnTick;
+
+    public Clock()
+    {
+        OnAlarm += Alarm;
+        OnTick += Tick;
+    }
+
+    public void Alarm(object sender, DateTime time)
+    {
+        Console.WriteLine("命定之刻" + time + "已至！");
+    }
+
+    public void Tick(object sender, DateTime time)
+    {
+        Console.WriteLine("现在时间是" + time);
+    }
+
+    public void Start()   //启动
+    {
+        while (true)
+        {
+            DateTime now = DateTime.Now;
+            OnTick(this, now);
+            if (now.ToString() == alarmTime.ToString())
+            {
+                OnAlarm(this, alarmTime);
+            }
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+
+    public void SetAlarmTime(DateTime atime)    //闹钟时间
+    {
+        Console.WriteLine(atime);
+        alarmTime = atime;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Clock clock = new Clock();
+        DateTime datetime = new DateTime();
+        datetime = DateTime.Now.AddSeconds(10);   //xx后响起
+        clock.SetAlarmTime(datetime);
+        clock.Start();
+    }
+}
---
+++ assignments4/ListPrint/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListPrint
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>    //泛型
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public Node<T> Head
+        {
+            get 
+            {
+                return head;
+            }
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = n;
+                tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> i = head; i != null; i = i.Next)
+            {
+                action(i.Data);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            GenericList<int> list = new GenericList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(rd.Next() % 100);   //随机数据测试
+            }
+            int sum = 0;
+            int maxn = Int32.MinValue;
+            int minn = Int32.MaxValue;
+
+            list.ForEach(n => Console.WriteLine(n));
+            list.ForEach(n => { maxn = maxn > n ? maxn : n; });
+            list.ForEach(n => { minn = minn < n ? minn : n; });
+            list.ForEach(n => sum += n);
+
+            Console.WriteLine("最大值是：" + maxn);
+            Console.WriteLine("最小值是：" + minn);
+            Console.WriteLine("求和是：" + sum);
+
+        }
+
+    }
+}
